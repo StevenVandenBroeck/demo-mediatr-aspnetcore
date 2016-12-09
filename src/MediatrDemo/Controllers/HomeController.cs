45f8923fe9308fc765620883c458bca866315b79
Steven Vanden Broeck <xytyx@telenet.be>
2016-12-09 16:21:22 +0100

    Async tests.

--- src/MediatrDemo/Controllers/HomeController.cs
+++ src/MediatrDemo/Controllers/HomeController.cs
@@ -23,14 +23,37 @@ namespace MediatrDemo.Controllers
             return View(_messageSource.Messages);
         }
 
-        public async Task<IActionResult> Publish()
+        public async Task<IActionResult> PublishEvent()
         {
             var notification = new DemoNotification() { Message = $"event published on {DateTime.Now}" };
             await _mediator.PublishAsync(notification);
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> Send()
+        public async Task<IActionResult> SendEvent()
+        {
+            var demoRequest = new DemoRequest() { Message = $"event sent on {DateTime.Now}" };
+            await _mediator.SendAsync(demoRequest);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> PublishAsyncEvent()
+        {
+            try
+            {
+                var notification = new DemoNotification() { Message = $"event published on {DateTime.Now}" };
+                _mediator.PublishAsync(notification);
+
+            }
+            catch ( Exception ex )
+            {
+                _messageSource.Add($"exception catched {ex.Message}");
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> SendAsyncEvent()
         {
             var demoRequest = new DemoRequest() { Message = $"event sent on {DateTime.Now}" };
             await _mediator.SendAsync(demoRequest);
--- src/MediatrDemo/Notifications/DemoNotificationHandler1.cs
+++ src/MediatrDemo/Notifications/DemoNotificationHandler1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 
@@ -15,9 +16,18 @@ namespace MediatrDemo.Notifications
 
         public Task Handle(DemoNotification notification)
         {
+            Task.Run(() => this.HandleInternal(notification));
+            return Task.CompletedTask;
+        }
+
+        private void HandleInternal(DemoNotification notification)
+        {
+            throw new Exception("error in Handler 1");
+
+            Thread.Sleep(5000);
+
             var msg = $"From {this.GetType().Name} - {notification.Message}";
             _messageSource.Add(msg);
-            return Task.FromResult(0);
         }
     }
 }
